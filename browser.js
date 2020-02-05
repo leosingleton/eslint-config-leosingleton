bd95dc492189f331ce7893c1234c38e3feea007e
Leo Singleton <leo@leosingleton.com>
2020-02-04 20:17:45 -0500

    Copying eslint config from FIM

---
+++ browser.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": [
+    "eslint-config-leosingleton"
+  ],
+  "env": {
+    "browser": true
+  }
+};
---
+++ common.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": [
+    "eslint-config-leosingleton"
+  ],
+  "env": {
+    "browser": true,
+    "node": true
+  }
+};
